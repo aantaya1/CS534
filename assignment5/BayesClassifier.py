256d071369bebaca8e64c6cbc2edfa033c5bab17
aantaya1 <antayaalexander@gmail.com>
2019-11-24 17:43:31 -0500

    wrapped up before assignm sub

--- assignment5/BayesClassifier.py
+++ assignment5/BayesClassifier.py
@@ -2,6 +2,7 @@ import csv
 import numpy as np
 import sys
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
 np.set_printoptions(threshold=sys.maxsize)
 
 
@@ -18,9 +19,6 @@ class BayesClassifier:
         # Total number of spam and ham messages
         self.num_spam = 0
         self.num_ham = 0
-
-        # Keep track of the probability of seeing each word
-        self.word_probability = dict()
 
         # For mapping between index and actual word
         self.mapper = dict()
@@ -52,7 +50,7 @@ class BayesClassifier:
 
                 # Iterate over every word on line
                 for idx in range(1, len(line)-2):
-                    feature.append(line[idx])
+                    feature.append(int(line[idx]))
 
                 features.append(feature)
 
@@ -80,20 +78,9 @@ class BayesClassifier:
             else:
                 num_ham += 1
 
-        words = dict()
-
         for row_index in range(0, len(X)):
             row = X[row_index]
             for idx in range(0, len(row)):
-
-                # Keep track of the total number of times that we have seen each word
-                if int(row[idx]) is 1:
-                    word = self.mapper.get(idx)
-
-                    if word not in words.keys():
-                        words[word] = 1
-                    else:
-                        words[word] = words[word] + 1
 
                 # If the word is present (1) and the message is spam (0)
                 if int(row[idx]) is 1 and int(y[row_index]) is 0:
@@ -129,9 +116,6 @@ class BayesClassifier:
             if word not in ham_occurrences.keys():
                 ham_occurrences[word] = 0
 
-            if word not in words:
-                words[word] = 0
-
             # Use laplas smoothing, where num classes is 2
             # Get probability of spam given that word
             word_spam_prob = (spam_occurrences[word] + self.k) / (num_spam + (self.k * 2))
@@ -141,35 +125,8 @@ class BayesClassifier:
             word_ham_prob = (ham_occurrences[word] + self.k) / (num_ham + (self.k * 2))
             self.ham_probabilities[word] = word_ham_prob
 
-            # Calculate the probability of each word (# of times we've seen it divided by the number of sentences)
-            # todo: not sure about this one... how many classes do we have here? Is it different than len(words)?
-            word_prob = (words[word] + self.k) / (len(words) + (self.k * 2))
-            self.word_probability[word] = word_prob
-
         self.num_ham = num_ham
         self.num_spam = num_spam
-
-        print('Number of SPAM: ' + str(num_spam))
-        print('SPAM Words: ' + str(spam_word))
-
-        print('Number of HAM: ' + str(num_ham))
-        print('HAM Words: ' + str(ham_word))
-
-        # print("Word Occurrences")
-        # print(words)
-        # print(self.word_probability)
-
-        # print('Spam Occurrences:')
-        # print(spam_occurrences)
-        #
-        # print('Ham Occurrences:')
-        # print(ham_occurrences)
-        #
-        # print("Spam Probabilities")
-        # print(self.spam_probabilities)
-        #
-        # print("Ham Probabilities")
-        # print(self.ham_probabilities)
 
     def predict(self, X):
 
@@ -182,40 +139,25 @@ class BayesClassifier:
         for row in X:
             probability_spam = 1
             probability_ham = 1
-            probability_word = 1
-
-            # print('Row: ' + str(row))
 
             # Multiple the probabilities of being spam or ham for each individual word (numerator)
             # Also calculate the probability of seeing each word (denominator)
             for idx in range(0, len(row)):
-                print('idx: '+str(idx))
-                if row[idx] is 0:
+
+                if int(row[idx]) is 0:
                     continue
 
                 word = self.mapper.get(idx)
 
                 if word in self.spam_probabilities:
-                    print('Found word (spam): ' + word)
                     probability_spam *= self.spam_probabilities[word]
 
                 if word in self.ham_probabilities:
-                    print('Found word (ham): ' + word)
                     probability_ham *= self.ham_probabilities[word]
-
-                if word in self.word_probability:
-                    print('Found word (word): ' + word)
-                    probability_word *= self.word_probability[word]
 
             # Multiply by the probability of having spam or ham
             probability_ham *= total_probability_ham
             probability_spam *= total_probability_spam
-
-            probability_ham /= probability_word
-            probability_spam /= probability_word
-
-            print('Probability HAM: ' + str(probability_ham))
-            print('Probability SPAM: ' + str(probability_spam))
 
             # Whichever probability is more likely will be the final classification
             if probability_ham >= probability_spam:
@@ -223,10 +165,54 @@ class BayesClassifier:
             else:
                 predicted_labels.append(0)
 
-        return predicted_labels
+        return np.array(predicted_labels)
+
+
+def calculate_f_score(predicted_labels, actual_labels):
+    num_tp = 0
+    num_tn = 0
+    num_fp = 0
+    num_fn = 0
+
+    if (len(predicted_labels) is not len(actual_labels)):
+        print("calculate_f_score(): Size of labels are not the same size!")
+        return -1
+
+    num_labels = len(predicted_labels)
+
+    # Go through every label and get the number of true positive, false positive, etc.
+    for idx in range(0, num_labels):
+        pred_val = int(predicted_labels[idx])
+        actual_val = int(actual_labels[idx])
+
+        if pred_val is 1:  # Positive values
+            if pred_val == actual_val:
+                num_tp += 1
+            elif pred_val != actual_val:
+                num_fp += 1
+        elif pred_val is 0:  # Negative values
+            if pred_val == actual_val:
+                num_tn += 1
+            elif pred_val != actual_val:
+                num_fn += 1
+
+    # Calculate pre and rec
+    pre = num_tp / (num_tp + num_fp)
+    rec = num_tp / (num_tp + num_fn)
+
+    # Calculate the actual f-measure now
+    f_measure = (2 * pre * rec) / (pre + rec)
+
+    return f_measure
 
 
 if __name__ == '__main__':
+
+    # How we will split the data set for training/testing
+    TRAINING_SPLIT = 0.8
+
+    ##### Use Subjects Dataset #####
+
     # Set k value for laplace smoothing
     bayes = BayesClassifier(k=1)
 
@@ -234,7 +220,39 @@ if __name__ == '__main__':
     X, y = bayes.feature_label_from_csv("dbworld_subjects_stemmed.csv")
 
     # Create 80/20 split for training/testing
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, train_size=0.8)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, train_size=TRAINING_SPLIT)
+
+    # Train/fit the model (i.e. calculate the probs for each word)
+    bayes.fit(X_train, y_train)
+
+    # Run prediction on test set (20% of original set)
+    labels = bayes.predict(X_test)
+
+    print('********My NB (Subjects)********')
+    print('Predicted: \t\t' + str(labels))
+    print('Actual:    \t\t' + str(y_test))
+    print('f-score:   \t\t' + str(calculate_f_score(labels, y_test)))
+
+    # Run same data sets with sklearn models
+    bayes_sklearn = MultinomialNB()
+    bayes_sklearn.fit(X_train, y_train)
+    labels = bayes_sklearn.predict(X_test)
+
+    print('********Sklearn NB (Subjects)********')
+    print('Predicted:\t\t' + str(labels))
+    print('Actual:   \t\t' + str(y_test))
+    print('f-score:   \t\t' + str(calculate_f_score(labels, y_test)))
+
+    ##### Use Bodies Dataset #####
+
+    # Set k value for laplace smoothing
+    bayes = BayesClassifier(k=1)
+
+    # Convert the csv to numpy array
+    X, y = bayes.feature_label_from_csv("dbworld_bodies_stemmed.csv")
+
+    # Create 80/20 split for training/testing
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, train_size=TRAINING_SPLIT)
 
     # Train/fit the model (i.e. calculate the probs for each word)
     bayes.fit(X_train, y_train)
@@ -242,8 +260,17 @@ if __name__ == '__main__':
     # Run prediction on test set (20% or original set)
     labels = bayes.predict(X_test)
 
-    print('Predicted: ' + str(labels))
-    print('Actual: ' + str(y_test))
+    print('********My NB (Bodies)********')
+    print('Predicted: \t\t' + str(labels))
+    print('Actual:    \t\t' + str(y_test))
+    print('f-score:   \t\t' + str(calculate_f_score(labels, y_test)))
 
-    # todo: calculate the f-score from assignment req
+    # Run same data sets with sklearn models
+    bayes_sklearn = MultinomialNB()
+    bayes_sklearn.fit(X_train, y_train)
+    labels = bayes_sklearn.predict(X_test)
 
+    print('********Sklearn NB (Bodies)********')
+    print('Predicted:\t\t' + str(labels))
+    print('Actual:   \t\t' + str(y_test))
+    print('f-score:   \t\t' + str(calculate_f_score(labels, y_test)))
